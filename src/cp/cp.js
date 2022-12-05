8a7ff26ad8270ef3770afec909f1e52c6ba3cd8f
Pavlov Kostyantyn <realanka77@gmail.com>
2022-12-05 17:38:46 +0200

    feat: new version subtack cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,11 @@
+import { spawn, fork } from "child_process";
+import path from "path";
+import { getDirname } from "../additional/funcDirname.js";
+
+const __dirname = getDirname(import.meta.url);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const cp = fork(path.resolve(__dirname, "files/script.js"), args);
 };
 
-spawnChildProcess();
+spawnChildProcess(["git", "log"]);
