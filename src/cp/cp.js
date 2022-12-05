082b9d13963096eb571063d211d1e1b8eb0476c3
Pavlov Kostyantyn <realanka77@gmail.com>
2022-12-05 17:46:48 +0200

    refacrot: clean code

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,11 +1,11 @@
-import { spawn, fork } from "child_process";
+import { fork } from "child_process";
 import path from "path";
 import { getDirname } from "../additional/funcDirname.js";
 
 const __dirname = getDirname(import.meta.url);
 
 const spawnChildProcess = async (args) => {
-  const cp = fork(path.resolve(__dirname, "files/script.js"), args);
+  fork(path.resolve(__dirname, "files/script.js"), args);
 };
 
-spawnChildProcess(["git", "log"]);
+spawnChildProcess(["I", "am", "learning", "node.js"]);
