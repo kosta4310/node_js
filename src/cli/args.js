f7c250b64259653f0e7be80ba21ced11584f79d8
Kostiantyn Pavlov <realanka77@gmail.com>
2022-12-07 20:53:31 +0200

    feat: add some modules

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,16 +1,12 @@
-const parseArgs = () => {
+export const parseArgs = () => {
     const arrArgv = process.argv.slice(2);
-    let temp = [];
-    const res = arrArgv.reduce((acc, curr) => {
-        if (curr.match(/^--\w+/)) curr = curr.slice(2);
-        temp.push(curr);
-        if (temp.length === 2) {
-            acc = [...acc, temp.join(' is ')];
-            temp = [];
-            return acc;
-        } else return acc;
-    }, []).join(', ');
-    console.log(res);
+    if (arrArgv.length) {
+      const [argument] = arrArgv;
+      const [_, name] = argument.split("=");
+      if (name) {
+        return name;
+      }
+    }
+    return 'unname';
 };
 
-parseArgs();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("hello");
+import { parseArgs } from './cli/args.js';
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+
+const name = parseArgs();
+const rl = readline.createInterface({ input, output });
+const answer = await rl.question('hello');
+console.log(answer);
+rl.close();
+
