1778659540db6781397b3a168986d36f03d738c2
Pavlov Kostyantyn <realanka77@gmail.com>
2022-12-10 22:24:25 +0200

    fix: fix name of outfile

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -5,11 +5,12 @@ import { createReadStream, createWriteStream } from "node:fs";
 
 export const compress = async (pathToFile, pathToDestination) => {
   const initialNameFile = path.basename(pathToFile);
-
-  const pathToFileOut = path.resolve(
-    pathToDestination,
-    `${initialNameFile}.br`
+  const newNameFile = initialNameFile.replace(
+    path.extname(initialNameFile),
+    ".br"
   );
+
+  const pathToFileOut = path.resolve(pathToDestination, newNameFile);
 
   const rs = createReadStream(pathToFile);
   const ws = createWriteStream(pathToFileOut, { flags: "wx" });
