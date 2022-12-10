08d0e6d3ad8bc9cb90fbc94efe6d620f221b3482
Pavlov Kostyantyn <realanka77@gmail.com>
2022-12-10 17:40:44 +0200

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -4,15 +4,21 @@ const { createHash } = await import("node:crypto");
 import { resolve } from "node:path";
 import { cwd } from "node:process";
 import { pipeline } from "node:stream/promises";
+import { Writable } from "node:stream";
 
+class MyStream extends Writable {
+  _write(chunk, encoding, cb) {
+    console.log(chunk.toString());
+    cb();
+  }
+}
 const calculateHash = async (pathToFile) => {
+  const out = new MyStream();
   const hash = createHash("sha256");
   const input = createReadStream(pathToFile);
-  await pipeline(input, hash.setEncoding("hex"), stdout);
-
-  // input.pipe(hash).setEncoding("hex").pipe(stdout);
+  await pipeline(input, hash.setEncoding("hex"), out);
 };
-
+// cd d:/download
 export async function calcHash(splittenLine) {
   const [_, ...path] = splittenLine;
   const [path_to_file] = path;
@@ -20,6 +26,8 @@ export async function calcHash(splittenLine) {
   if (!path || path.length > 1) {
     console.log("Invalid input");
   } else {
-    await calculateHash(pathToFile).catch((err) => console.log(err));
+    await calculateHash(pathToFile).catch(() =>
+      console.log("Operation failed")
+    );
   }
 }
