0719f32f174648ecd68c58c11d967d8f30965b33
Kostiantyn Pavlov <realanka77@gmail.com>
2022-12-15 22:50:17 +0200

    revert

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import http from "node:http";
 import url from "node:url";
 import fs from "node:fs";
-// import { getDirname } from "./utils/getDirname.js";
+import { getDirname } from "./utils/getDirname";
 
-// const __dirname = getDirname(import.meta.url);
+const __dirname = getDirname(import.meta.url);
 
-// console.log(__dirname);
+console.log(__dirname);
 console.log("hello from master");
 
 const server = http
