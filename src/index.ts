67d679cbf08e10dfbef31a832f999fee3653a6cc
Kostiantyn Pavlov <realanka77@gmail.com>
2022-12-16 00:09:30 +0200

    feat: add prettier, eslint

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-import http from "node:http";
-import url from "node:url";
-import fs from "node:fs";
-import { getDirname } from "./utils/getDirname";
+import http from 'node:http';
+import url from 'node:url';
+import fs from 'node:fs';
+import { getDirname } from './utils/getDirname.js';
 
 const __dirname = getDirname(import.meta.url);
+const me: string = 'dfkdjf';
 
 console.log(__dirname);
-console.log("hello from master");
+console.log('hello from master');
 
 const server = http
   .createServer((req, res) => {
-    res.end("hello from server");
+    res.end('hello from server');
   })
-  .listen(4000, () => console.log("server is running on port 4000"));
+  .listen(4000, () => console.log('server is running on port 4000'));
 
-server.on("error", () => console.log("error"));
-server.on("close", () => console.log("close"));
+server.on('error', () => console.log('error'));
+server.on('close', () => console.log('close'));
