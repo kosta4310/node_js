2ca88ec3eff045e19149968d29d6e79d52460ae2
Pavlov Kostyantyn <realanka77@gmail.com>
2023-01-16 12:20:24 +0200

    feat: add a correct close app

--- src/index.ts
+++ src/index.ts
@@ -78,6 +78,9 @@ async function startApp() {
   } else {
     const appServer = await createWorkerServer();
     appServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    process.on('SIGINT', () => {
+      appServer.close();
+    });
   }
 }
 
